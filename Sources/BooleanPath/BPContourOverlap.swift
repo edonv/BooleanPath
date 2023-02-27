a6d80cc27f36276ba39e953e316a452f46f6f8d2
Edon <edon@valdman.works>
2023-02-26 21:44:56 -0600

    Update BPContourOverlap.swift
    Updated with comments and details from VectorBoolean repo, while also cleaning up the formatting

--- Sources/BooleanPath/BPContourOverlap.swift
+++ Sources/BooleanPath/BPContourOverlap.swift
@@ -17,7 +17,11 @@ import Foundation
 class BPContourOverlap {
     fileprivate var runs: [BPEdgeOverlapRun] = []
     
-    func addOverlap(_ range: BPBezierIntersectRange, forEdge1 edge1: BPBezierCurve, edge2: BPBezierCurve) {
+    // + (id) contourOverlap
+    // Use t = FBContourOverlap() instead
+    
+    //- (void) addOverlap:(FBBezierIntersectRange *)range forEdge1:(FBBezierCurve *)edge1 edge2:(FBBezierCurve *)edge2
+    public func addOverlap(_ range: BPBezierIntersectRange, forEdge1 edge1: BPBezierCurve, edge2: BPBezierCurve) {
         let overlap = BPEdgeOverlap(range: range, edge1: edge1, edge2: edge2)
         
         var createNewRun = false
@@ -42,34 +46,42 @@ class BPContourOverlap {
         }
     }
     
-    func doesContainCrossing(_ crossing: BPEdgeCrossing) -> Bool {
+    //- (BOOL) doesContainCrossing:(FBEdgeCrossing *)crossing
+    public func doesContainCrossing(_ crossing: BPEdgeCrossing) -> Bool {
         if runs.count == 0 {
             return false
         }
+        
         for run in runs {
             if run.doesContainCrossing(crossing) {
                 return true
             }
         }
+        
         return false
     }
     
-    func doesContainParameter(_ parameter: Double, onEdge edge: BPBezierCurve) -> Bool {
+    //- (BOOL) doesContainParameter:(CGFloat)parameter onEdge:(FBBezierCurve *)edge
+    public func doesContainParameter(_ parameter: Double, onEdge edge: BPBezierCurve) -> Bool {
         if runs.count == 0 {
             return false
         }
+        
         for run in runs {
             if run.doesContainParameter(parameter, onEdge: edge) {
                 return true
             }
         }
+        
         return false
     }
     
-    func runsWithBlock(_ block: (_ run: BPEdgeOverlapRun) -> Bool) {
+    //- (void) runsWithBlock:(void (^)(FBEdgeOverlapRun *run, BOOL *stop))block
+    public func runsWithBlock(_ block: (_ run: BPEdgeOverlapRun) -> Bool) {
         if runs.count == 0 {
             return
         }
+        
         for run in runs {
             let stop = block(run)
             if stop {
@@ -78,46 +90,60 @@ class BPContourOverlap {
         }
     }
     
-    func reset() {
+    //- (void) reset
+    public func reset() {
         if runs.count == 0 {
             return
         }
         runs.removeAll()
     }
     
-    var isComplete: Bool {
+    //- (BOOL) isComplete
+    public var isComplete: Bool {
         if runs.count == 0 {
             return false
         }
+        
+        // To be complete, we should have exactly one run that wraps around
         if runs.count != 1 {
             return false
         }
+        
         return runs[0].isComplete
     }
     
-    var isEmpty: Bool {
+    //- (BOOL) isEmpty
+    public var isEmpty: Bool {
         return runs.count == 0
     }
     
+    //@property (readonly) FBBezierContour *contour1;
+    //- (FBBezierContour *) contour1
     var contour1: BPBezierContour? {
         if runs.count == 0 {
             return nil
         }
+        
         let run = runs[0]
         return run.contour1
     }
     
+    //@property (readonly) FBBezierContour *contour2;
+    //- (FBBezierContour *) contour2
     var contour2: BPBezierContour? {
         if runs.count == 0 {
             return nil
         }
+        
         let run = runs[0]
         return run.contour2
     }
     
-    func isBetweenContour(_ contour1: BPBezierContour, andContour contour2: BPBezierContour) -> Bool {
+    //- (BOOL) isBetweenContour:(FBBezierContour *)contour1 andContour:(FBBezierContour *)contour2
+    public func isBetweenContour(_ contour1: BPBezierContour, andContour contour2: BPBezierContour) -> Bool {
         let myContour1 = self.contour1
         let myContour2 = self.contour2
+        
         return (contour1 === myContour1 && contour2 === myContour2)
             || (contour1 === myContour2 && contour2 === myContour1)
     }

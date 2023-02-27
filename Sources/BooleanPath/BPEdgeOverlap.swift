d08f9fc1fb6e2e6afd0a3ec9aac7b47d47cf9ea0
Edon <edon@valdman.works>
2023-02-26 22:50:11 -0600

    Update BPEdgeOverlap.swift
    Updated with comments and details from VectorBoolean repo, while also cleaning up the formatting

--- Sources/BooleanPath/BPEdgeOverlap.swift
+++ Sources/BooleanPath/BPEdgeOverlap.swift
@@ -29,29 +29,37 @@ class BPEdgeOverlap {
         self.edge2 = edge2
     }
     
+    //- (BOOL) fitsBefore:(FBEdgeOverlap *)nextOverlap
     func fitsBefore(_ nextOverlap: BPEdgeOverlap) -> Bool {
         if ProximityMath.areValuesClose(range.parameterRange1.upperBound, value2: 1.0, threshold: BPEdgeOverlap.threshold) {
+            // nextOverlap should start at 0 of the next edge
             let nextEdge = edge1.next
+            
             return nextOverlap.edge1 == nextEdge
                 && ProximityMath.areValuesClose(nextOverlap.range.parameterRange1.lowerBound, value2: 0.0, threshold: BPEdgeOverlap.threshold)
         }
         
+        // nextOverlap should start at about maximum on the same edge
         return nextOverlap.edge1 == edge1
             && ProximityMath.areValuesClose(nextOverlap.range.parameterRange1.lowerBound, value2: range.parameterRange1.upperBound, threshold: BPEdgeOverlap.threshold)
     }
     
+    //- (BOOL) fitsAfter:(FBEdgeOverlap *)previousOverlap
     func fitsAfter(_ previousOverlap: BPEdgeOverlap) -> Bool {
         if ProximityMath.areValuesClose(range.parameterRange1.lowerBound, value2: 0.0, threshold: BPEdgeOverlap.threshold) {
+            // previousOverlap should end at 1 of the previous edge
             let previousEdge = edge1.previous
             
             return previousOverlap.edge1 == previousEdge
                 && ProximityMath.areValuesClose(previousOverlap.range.parameterRange1.upperBound, value2: 1.0, threshold: BPEdgeOverlap.threshold)
         }
         
+        // previousOverlap should end at about the minimum on the same edge
         return previousOverlap.edge1 == edge1
             && ProximityMath.areValuesClose(previousOverlap.range.parameterRange1.upperBound, value2: range.parameterRange1.lowerBound, threshold: BPEdgeOverlap.threshold)
     }
     
+    //- (void) addMiddleCrossing
     func addMiddleCrossing() {
         let intersection = _range.middleIntersection
         
@@ -68,11 +76,16 @@ class BPEdgeOverlap {
         edge2.addCrossing(theirCrossing)
     }
     
+    //- (BOOL) doesContainParameter:(CGFloat)parameter onEdge:(FBBezierCurve *)edge startExtends:(BOOL)extendsBeforeStart endExtends:(BOOL)extendsAfterEnd
     func doesContainParameter(_ parameter: Double,
                               onEdge edge:BPBezierCurve,
                               startExtends extendsBeforeStart: Bool,
                               endExtends extendsAfterEnd: Bool) -> Bool {
+        // By the time this is called, we know the crossing is on one of our edges.
         if extendsBeforeStart && extendsAfterEnd {
+            // The crossing is on the edge somewhere,
+            // and the overlap extends past this edge in both directions,
+            // so it's safe to say the crossing is contained
             return true
         }
         
